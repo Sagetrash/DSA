c5c3862695cce269055fee43baf04363c05bf830
sagetrash <xded037@gmail.com>
2025-09-21 10:36:01 +0530

    if a number is prime or not

--- module-4/isprime.java
+++ module-4/isprime.java
@@ -12,16 +12,17 @@ public class isprime {
     }
     
     public static boolean is_prime(int n){
-        int count = 0;
-        for(int i = 1; i <=n; i++){
-            if(n%i == 0){
-                count += 1;
-            }
-        }
-        if(count == 2){
+        if(n==1){
+            return false;
+        }else if(n==2 | n == 3 ){
             return true;
         }else{
-            return false;
+            for(int i = 2; i <= Math.sqrt(n);i++){
+                if(n%i == 0){
+                    return false;
+                }
+            }
         }
+        return true;
     }
 }
