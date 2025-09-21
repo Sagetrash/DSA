8f2b6dff1535dca9f8c0009ec001efca097fe31a
sagetrash <xded037@gmail.com>
2025-09-21 11:43:10 +0530

    can now take very long values, and provide the factors of compostite numbers

--- module-4/isprime.java
+++ module-4/isprime.java
@@ -3,15 +3,16 @@ import java.util.*;
 public class isprime {
     public static void main(String args []){
         Scanner sc = new Scanner(System.in);
-        int input_num = sc.nextInt(); sc.close();
+        long input_num = sc.nextLong(); sc.close();
         if(is_prime(input_num)){
             System.out.println("the number is a prime number.");
         }else{
+            System.out.println(factors(input_num));
             System.out.println("the number is not a prime number.");
         }
     }
     
-    public static boolean is_prime(int n){
+    public static boolean is_prime(long n){
         if(n==1){
             return false;
         }else if(n==2 | n == 3 ){
@@ -24,5 +25,16 @@ public class isprime {
             }
         }
         return true;
+    }
+
+    public static List<Long> factors(long n){
+        List<Long> facts = new ArrayList<>();
+        for(long i = 2; i <= Math.sqrt(n); i++){
+            if(n%i == 0){
+                facts.add(i);
+                if(!facts.contains(n/i)){facts.add(n/i);};
+            }
+        }
+        return facts;
     }
 }
